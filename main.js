3ffc7c57992dba70193376ddffaccb29c285f3c0
Ayyub2311 <157877827+Ayyub2311@users.noreply.github.com>
2024-03-22 18:23:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$addClass('h1', 'red')
+$removeClass('h2', 'red')
+$toggleClass('h3', 'red')
+// $toggleClassNew('button', '.box', 'active')
+
+$toggleClassNew('.burger', 'nav', 'active')
+$toggleClassNew('.burger', 'nav', 'nav-active')
+
+
+
